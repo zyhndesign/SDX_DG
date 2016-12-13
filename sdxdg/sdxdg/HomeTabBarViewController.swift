2f35b2b75307a29e7286855805e36dfea70affe8
zyhndesign <zyhndesign@zyhnmatoMacBook-Air.local>
2016-12-13 17:25:28 +0800

    init view

---
+++ sdxdg/sdxdg/HomeTabBarViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeTabBarViewController.swift
+//  sdxdg
+//
+//  Created by lotusprize on 16/12/13.
+//  Copyright © 2016年 geekTeam. All rights reserved.
+//
+
+
+import UIKit
+
+class HomeTabBarViewController : UITabBarController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- sdxdg/sdxdg/LoginViewController.swift
+++ sdxdg/sdxdg/LoginViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    let yellowColor:CGColor = UIColor.init(red:253/255.0, green: 220/255.0, blue: 56/255.0, alpha: 1.0).cgColor
+    let blackColor:UIColor = UIColor.init(red: 59/255.0, green: 59/255.0, blue: 59/255.0, alpha: 1.0)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        initTextFiledStyle(textField:username)
+        initTextFiledStyle(textField:password)
+        
+        loginBtn.layer.cornerRadius = 6
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,17 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func loginBtnClick(_ sender: Any) {
+        
+        self.present(HomeTabBarViewController(), animated: true, completion: nil)
+    }
     
+    func initTextFiledStyle(textField tField: UITextField) {
+        tField.layer.cornerRadius = 6
+        tField.layer.borderColor = yellowColor
+        tField.layer.borderWidth = 1.0
+        tField.backgroundColor = blackColor
+        tField.setValue(UIColor.white, forKeyPath: "_placeholderLabel.textColor")
+    }
 }
 
