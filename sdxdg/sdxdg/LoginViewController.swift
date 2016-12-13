7df24bcfc1c086a02cda4483c9c2cd2b32128400
zyhndesign <zyhndesign@zyhnmatoMacBook-Air.local>
2016-12-13 15:45:16 +0800

    project init

---
+++ sdxdg/sdxdg/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  sdxdg
+//
+//  Created by lotusprize on 16/12/13.
+//  Copyright © 2016年 geekTeam. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
