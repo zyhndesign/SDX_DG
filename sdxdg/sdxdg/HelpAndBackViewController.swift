25543c5594fe27bdfc1cfb24789298f16002b410
zyhndesign <zyhndesign@zyhnmatoMacBook-Air.local>
2017-04-26 17:23:15 +0800

    增加修改密码模块

--- sdxdg/sdxdg/HelpAndBackViewController.swift
+++ sdxdg/sdxdg/HelpAndBackViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class HelpAndBackViewController: UIViewController {
     
     @IBOutlet var textView: UITextView!
     @IBOutlet var postBtn: UIButton!
+    
+    let userId = LocalDataStorageUtil.getUserIdFromUserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +28,35 @@ class HelpAndBackViewController: UIViewController {
     }
     
     @IBAction func postDataBtnClick(_ sender: Any) {
+        if textView.text.isEmpty{
+            MessageUtil.showMessage(view: self.view, message: "请输入反馈内容")
+            return
+        }
         
+        let parameters:Parameters = ["userId":userId,"info":textView.text]
+        
+        let hud = MBProgressHUD.showAdded(to: self.view, animated: true)
+        hud.label.text = "提交中..."
+        Alamofire.request(ConstantsUtil.APP_USER_BACK_HELP_DATA_URL,method:.post,parameters:parameters).responseJSON { (response) in
+            
+            if let data = response.result.value {
+                let responseResult = JSON(data)
+                
+                let resultCode = responseResult["resultCode"].intValue
+                
+                if resultCode == 200{
+                    hud.label.text = "操作成功"
+                    hud.hide(animated: true, afterDelay: 1.0)
+                    
+                    self.navigationController?.popViewController(animated: true)
+                    
+                }
+                else{
+                    hud.label.text = "操作失败"
+                    hud.hide(animated: true, afterDelay: 1.0)
+                }
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
--- sdxdg/sdxdg/UpdatePwdViewController.swift
+++ sdxdg/sdxdg/UpdatePwdViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class UpdatePwdViewController: UIViewController {
     
@@ -15,6 +17,9 @@ class UpdatePwdViewController: UIViewController {
     @IBOutlet var newPwdTextField2: UITextField!
     @IBOutlet var postBtn: UIButton!
     
+    let userId = LocalDataStorageUtil.getUserIdFromUserDefaults()
+    let username = LocalDataStorageUtil.getUserInfoByKey(key: "username")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,7 +27,54 @@ class UpdatePwdViewController: UIViewController {
     }
     
     @IBAction func postBtnClick(_ sender: Any) {
+        if (pwdTextField.text?.isEmpty)!{
+            MessageUtil.showMessage(view: self.view, message: "请输入旧密码")
+            return
+        }
         
+        if (newPwdTextField1.text?.isEmpty)!{
+            MessageUtil.showMessage(view: self.view, message: "请输入新密码")
+            return
+        }
+        
+        if (newPwdTextField2.text?.isEmpty)!{
+            MessageUtil.showMessage(view: self.view, message: "请确认新密码")
+            return
+        }
+        
+        if newPwdTextField1.text != newPwdTextField2.text{
+            MessageUtil.showMessage(view: self.view, message: "两次输入密码不相等周云")
+            return
+        }
+        
+        let parameters:Parameters = ["userId":userId,"password":pwdTextField.text!, "newPwd":newPwdTextField1.text!,"username":username]
+        
+        let hud = MBProgressHUD.showAdded(to: self.view, animated: true)
+        hud.label.text = "提交中..."
+        Alamofire.request(ConstantsUtil.ALL_USER_UPDATE_PWD,method:.post,parameters:parameters).responseJSON { (response) in
+            
+            if let data = response.result.value {
+                let responseResult = JSON(data)
+                
+                let resultCode = responseResult["resultCode"].intValue
+                
+                if resultCode == 200{
+                    hud.label.text = "操作成功"
+                    hud.hide(animated: true, afterDelay: 1.0)
+                    
+                    self.navigationController?.popViewController(animated: true)
+                    
+                }
+                else if resultCode == 300{
+                    hud.label.text = "旧密码错误"
+                    hud.hide(animated: true, afterDelay: 1.0)
+                }
+                else{
+                    hud.label.text = "操作失败"
+                    hud.hide(animated: true, afterDelay: 1.0)
+                }
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
